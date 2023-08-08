eaf179ea4739fb9b0f96b3da39f08a823c0a53d1
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2023-08-07 21:14:42 -0400

    Begin project

---
+++ PublixDeliNutritionalCalc.CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PublixDeliNutritionalCalc.CLI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.ReadLine();
+        }
+    }
+}
