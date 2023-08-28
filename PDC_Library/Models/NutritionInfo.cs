c5c9e32d1b439f487a73a7853af3c3b8776610cd
Kevin Foughty <97193605+metalBread33@users.noreply.github.com>
2023-08-28 13:01:41 -0400

    Begin work on Base Model, variables, constructor and ToString added

---
+++ PDC_Library/Models/NutritionInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PDC_Library.Models
+{
+    internal class NutritionInfo   //Stores the nutritional information for items
+    {
+        public uint Calories { get; set; }
+        public uint Carbs { get; set; }
+        public uint Fat { get; set; }
+        public uint Protein { get; set; }
+        public uint SaturatedFat { get; set; }
+        public uint UnSaturatedFat { get; set; }
+        public uint Sodium { get; set; }
+        public uint Cholesterol { get; set; }
+
+        public NutritionInfo(uint cal, uint carbs, uint fat, uint protein,
+            uint satFat, uint uFat, uint sodium, uint cholesterol) 
+        {
+            Calories = cal;
+            Carbs = carbs;
+            Fat = fat;
+            Protein = protein;
+            SaturatedFat = satFat;
+            UnSaturatedFat = uFat;
+            Sodium = sodium;
+            Cholesterol = cholesterol;
+        }
+
+        public override string ToString()
+        {
+            return $"Calories: {Calories}\nCarbs: {Carbs}g\nFat: {Fat}g" +
+                $"\n\tSaturated Fat: {SaturatedFat}g\n\tUnsaturated Fat:" +
+                $" {UnSaturatedFat}g\nProtein: {Protein}g\nSodium: {Sodium}mg" +
+                $"\nCholesterol: {Cholesterol}mg";
+        }
+    }
+}
